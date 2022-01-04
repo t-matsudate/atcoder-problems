58dbb169b90c0e11c4d30e285133365b0d71b06c
t-matsudate <t.matsudate@gmail.com>
2022-01-04 16:06:06 +0900

    Added: It solved the Practice A.

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+pub mod problems;
--- src/problems/beginners/practice/problem_a.rs
+++ src/problems/beginners/practice/problem_a.rs
@@ -42,3 +42,41 @@ pub fn main() -> IOResult<()> {
 pub fn problem_a(a: i32, b: i32, c: i32, s: &str) -> (i32, &str) {
     (a + b + c, s)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        time::{
+            SystemTime,
+            SystemTimeError
+        }
+    };
+    use quickcheck::TestResult;
+    use super::*;
+
+    #[quickcheck]
+    fn test_problem_a(a: i32, b: i32, c: i32, s: String) -> TestResult {
+        let s_len = s.len();
+
+        if !((1 <= a && a <= 1000) && (1 <= b && b <= 1000) && (1 <= c && c <= 1000) && (1 <= s_len && s_len <= 100)) {
+            TestResult::discard()
+        } else if problem_a(a, b, c, s.as_str()) != (a + b + c, s.as_str()) {
+            TestResult::failed()
+        } else {
+            TestResult::passed()
+        }
+    }
+
+    #[test]
+    fn test_execution_time() -> Result<(), SystemTimeError> {
+        let time_start = SystemTime::now();
+
+        problem_a(1, 1, 1, &"me");
+
+        let time_end = SystemTime::now();
+        let time_diff = time_end.duration_since(time_start)?;
+
+        assert!(time_diff.as_secs() <= 2, "The code of Practice A exceeded the time limit!");
+        Ok(())
+    }
+}
